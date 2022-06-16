dccc47eb253753b9dd9b95937b0a1d82deb39211
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2022-06-16 14:34:35 +0200

    Test pwm, Dockerfile fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python
 WORKDIR /home/python/app
 COPY Pine64RGBController.py .
-RUN git clone https://github.com/swkim01/RPi.GPIO-PineA64.git
+RUN python -m pip install --upgrade pip
 RUN pip install RPi.GPIO
 CMD python Pine64RGBController.py
--- Pine64RGBController.py
+++ Pine64RGBController.py
@@ -1,1 +1,22 @@
-print("Hello")
+import RPi.GPIO as GPIO
+from time import sleep
+
+R_PIN = 11
+G_PIN = 13
+B_PIN = 15
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(R_PIN, GPIO.OUT)
+pwm = GPIO.PWM(R_PIN,1000)
+pwm.start(0)
+
+while True:
+    for duty in range(0,101,1):
+        pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+        sleep(0.01)
+    sleep(0.5)
+    
+    for duty in range(100,-1,-1):
+        pwm.ChangeDutyCycle(duty)
+        sleep(0.01)
+    sleep(0.5)
